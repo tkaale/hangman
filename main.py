01ca77c26c0cf601a46776d4a5c2667cd2af2ce8
Aleksandra <tkaczyk.aleksandra@outlook.com>
2022-09-09 16:23:48 +0200

    .

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import ascii_graphics, data_menager, random
 
-ALPHABET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+#ALPHABET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 def pick_random_word(filename):
     words_list = data_menager.get_table_from_file(filename)
@@ -15,16 +15,28 @@ def check_user_letter():
             if len(user_letter) == 1:
                 break
             else:
-                print('This have to be just ONE letter')
+                print('This have to be just ONE letter.')
                 continue
         else:
-            print('Not a letter')
+            print('Not a letter.')
             continue
+    return user_letter
+
+def check_letter_to_word(word, user_letter):
+    word_list = list(word.strip(''))
+    if user_letter.lower() in word_list:
+        return True
+    else:
+        return False
+
+
+
 
 def main():
     word = pick_random_word('hangman/words.csv')
-    check_user_letter()
-
+    print(word)
+    user_letter = check_user_letter()
+    print(check_letter_to_word(word, user_letter))
     
 if __name__=='__main__':
     main()
