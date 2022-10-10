885ce42b59381396b8fb120378edcb257d1970ec
Aleksandra <tkaczyk.aleksandra@outlook.com>
2022-10-10 13:46:39 +0200

    display '_' on the letters

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from distutils.command import check
 import ascii_graphics, data_menager, random
 
 #ALPHABET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 def pick_random_word(filename):
     words_list = data_menager.get_table_from_file(filename)
-    random_word = words_list[random.randint(0, len(words_list) - 1)]
+    random_word = random.choice(words_list)
     return random_word
 
 
@@ -26,21 +25,23 @@ def check_user_letter():
 
 def check_letter_to_word(word, user_letter):
     word_list = list(word.strip(''))
-    # if user_letter.lower() in word_list:
-    #     return True
-    # else:
-    #     return False
     for letter in word_list:
         if letter == user_letter.lower():
             print('Right')
         else:
             print('Wrong')
 
+def display(word):
+    display_list = []
+    for letter in word:
+        display_list.append('_')
+    print(' '.join(display_list))
 
 
 def main():
     word = pick_random_word('words.csv') # wybiera losowe słowo z pliku csv
-    print(word)
+    print(word) #checking
+    display(word)
     user_letter = check_user_letter() # sprawdza poprawność wpisanej danej
     check_letter_to_word(word, user_letter) # printuje false/true w zależności czy dana litera jest w słowie
     
